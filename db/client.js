feae131d03ee98d6aa716c40396cc13329274c79
Mayank Awasthi <mayankawasthi20@gmail.com>
2022-03-19 12:26:22 -0400

    client change

--- db/client.js
+++ db/client.js
@@ -1,6 +1,11 @@
 // build and export your unconnected client here
 const { Client } = require('pg');
 
-const client = new Client(process.env.DATABASE_URL || "postgres://localhost:5432/fitness-dev");
+// const client = new Client(process.env.DATABASE_URL || "postgres://localhost:5432/fitness-dev");
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/fitness-dev',
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+  });
 
 module.exports = client
